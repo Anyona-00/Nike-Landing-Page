3a6cee13ef22944ab44818db8be9b64b40cb5b93
Anyona-00 <ombasaanyona@gmail.com>
2024-07-13 18:31:51 +0300

    Initial Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SuperQuality,
+  Footer,
+  SpecialOffer,
+  Subscribe,
+  CustomerReviews,
+};
